0da4ee75e9993a892a6558d78d7ab33e3ac7ba07
Mario Jakopec <mario@faveo.hr>
2013-06-14 10:40:04 +0200

    Update secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infinum::Application.config.secret_token = '2895cf7b5505b01acf480422ff6887e474bf46aa4edd139a33ec7b44fb02bcd07841631a625d57652670c8739b8b9d5ac919052cd25f5402ab6881d348278e73'
